8fd13dadd7eb266c6d18a7d280e5f0a2df2f73c6
Netto Farias <olfnetto@gmail.com>
2025-11-17 03:06:53 -0300

    atual

--- baileys/index.js
+++
@@ -1,8 +0,0 @@
-// Placeholder do conector Baileys
-// Aqui você vai implementar:
-// - Conexão WhatsApp
-// - Recebimento de mensagens
-// - Envio para Chatwoot
-// - Webhooks para n8n
-
-console.log("Baileys conector iniciado...");
